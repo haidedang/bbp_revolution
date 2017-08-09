8facd7996037d2d42b56399f89034ec856dfd077
Hai Dang <haiduc.dang91@gmail.com>
2017-08-09 23:22:13 +0200

    Demo

--- app.js
+++ app.js
@@ -79,24 +79,11 @@ app.use(function (req, res, next) {
 app.use('/', routes);
 app.use('/users', users);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+// Set Port
+app.set('port', (process.env.PORT || 3000));
+
+app.listen(app.get('port'), function(){
+    console.log('Server started on port ' + app.get('port'));
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-
 
 module.exports = app;
